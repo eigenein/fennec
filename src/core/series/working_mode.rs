6f0fcc183180feb6492b70468ca031c147d43bef
Pavel Perestoronin <eigenein@gmail.com>
2025-09-21 21:28:44 +0200

    Oopsie, fix the conditional clause

--- src/core/series/working_mode.rs
+++ src/core/series/working_mode.rs
@@ -21,7 +21,7 @@ impl<I: Copy> Series<WorkingMode, I> {
         (self[index_1], self[index_2]) = loop {
             let new_1 = fastrand::choice(Self::MODES).unwrap();
             let new_2 = fastrand::choice(Self::MODES).unwrap();
-            if (new_1, new_2) != (self[index_1], self[index_2]) {
+            if new_1 != self[index_1] || new_2 != self[index_2] {
                 break (new_1, new_2);
             }
         };

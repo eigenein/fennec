f474121a8681598c4b620851819a97c40ea10b89
Pavel Perestoronin <eigenein@gmail.com>
2025-10-13 13:03:18 +0200

    Remove dead code

--- src/core/working_mode.rs
+++ src/core/working_mode.rs
@@ -1,21 +1,14 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Copy, Clone, Debug, Default, Eq, PartialEq, Ord, PartialOrd, Serialize, Deserialize)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum WorkingMode {
     /// Do not do anything.
-    #[serde(alias = "I", alias = "M", alias = "R")]
     Idle,
 
-    /// Forced charging on any power.
-    #[serde(alias = "C")]
+    /// Forced charging from any source.
     Charging,
 
     /// Forced discharging, no matter the actual consumption.
-    #[serde(alias = "D")]
     Discharging,
 
     /// Charge on excess PV power, discharge on insufficient PV power.
-    #[default]
-    #[serde(alias = "B")]
     Balancing,
 }

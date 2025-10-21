68152b74c66cad0bf4e2069d7ba997971ce82fcc
Pavel Perestoronin <eigenein@gmail.com>
2025-10-21 14:30:57 +0200

    Implement `peak_hourly`

--- src/core/series/aggregate.rs
+++ src/core/series/aggregate.rs
@@ -1,8 +1,4 @@
-use std::{
-    hash::Hash,
-    iter::Sum,
-    ops::{Add, Div},
-};
+use std::ops::{Add, Div};
 
 use chrono::Timelike;
 use itertools::Itertools;
@@ -13,8 +9,8 @@ pub trait AggregateHourly {
     fn average_hourly<K, V>(self) -> [Option<V>; 24]
     where
         Self: Sized + Iterator<Item = (K, V)>,
-        K: Hash + Eq + Timelike,
-        V: Copy + Sum + Add<V, Output = V> + Div<f64, Output = V>,
+        K: Timelike,
+        V: Copy + Add<V, Output = V> + Div<f64, Output = V>,
     {
         let mut sums = [None; 24];
         let mut weights = [0_u32; 24];
@@ -29,6 +25,21 @@ pub trait AggregateHourly {
             .collect_array()
             .unwrap()
     }
+
+    fn peak_hourly<K, V>(self) -> [Option<V>; 24]
+    where
+        Self: Sized + Iterator<Item = (K, V)>,
+        K: Timelike,
+        V: Copy + PartialOrd,
+    {
+        let mut peaks: [Option<V>; 24] = [None; 24];
+        for (timestamp, value) in self {
+            let hour = timestamp.hour() as usize;
+            peaks[hour] =
+                Some(peaks[hour].map_or(value, |peak| if value > peak { value } else { peak }));
+        }
+        peaks
+    }
 }
 
 #[cfg(test)]
@@ -39,12 +50,19 @@ mod tests {
 
     #[test]
     fn test_average_hourly() {
-        let series = vec![
-            (NaiveTime::from_hms_opt(23, 0, 0).unwrap(), 10.0),
-            (NaiveTime::from_hms_opt(23, 0, 0).unwrap(), 2.0),
-        ];
+        let time = NaiveTime::from_hms_opt(23, 0, 0).unwrap();
+        let series = vec![(time, 10.0), (time, 2.0)];
         let averages = series.into_iter().average_hourly();
         assert_eq!(&averages[0..23], [None; 23]);
         assert_eq!(averages[23], Some(6.0));
+    }
+
+    #[test]
+    fn test_peak_hourly() {
+        let time = NaiveTime::from_hms_opt(23, 0, 0).unwrap();
+        let series = vec![(time, 2), (time, 3), (time, 1)];
+        let averages = series.into_iter().peak_hourly();
+        assert_eq!(&averages[0..23], [None; 23]);
+        assert_eq!(averages[23], Some(3));
     }
 }

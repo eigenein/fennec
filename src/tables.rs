7310486a4caf3a60a6b288343aa8bc3a97708575
Pavel Perestoronin <eigenein@gmail.com>
2025-11-17 19:51:29 +0100

    Fix: `DarkGreen` screws up `k9s`

--- src/tables.rs
+++ src/tables.rs
@@ -59,8 +59,7 @@ pub fn build_steps_table(
                 },
             ),
             Cell::new(format!("{:?}", step.working_mode)).fg(match step.working_mode {
-                CoreWorkingMode::ChargeSlowly => Color::DarkGreen,
-                CoreWorkingMode::Charge => Color::Green,
+                CoreWorkingMode::Charge | CoreWorkingMode::ChargeSlowly => Color::Green,
                 CoreWorkingMode::Discharge => Color::Red,
                 CoreWorkingMode::Balance => Color::DarkYellow,
                 CoreWorkingMode::Backup => Color::Magenta,

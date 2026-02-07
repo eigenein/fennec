64585c4e0141e358000d6ff4ca8bfe8843cf75eb
Pavel Perestoronin <eigenein@gmail.com>
2026-02-07 13:03:54 +0100

    Simplify `SentryArgs::init`

--- fennec-cli/src/cli/sentry.rs
+++ fennec-cli/src/cli/sentry.rs
@@ -8,28 +8,24 @@ use crate::prelude::*;
 #[derive(Parser)]
 pub struct SentryArgs {
     #[clap(long = "sentry-dsn", env = "SENTRY_DSN")]
-    sentry_dsn: Option<String>,
+    dsn: Option<String>,
 }
 
 impl SentryArgs {
-    pub fn init(&self) -> Option<ClientInitGuard> {
-        self.sentry_dsn.as_deref().map_or_else(
-            || {
-                warn!("Sentry is not configured");
-                None
-            },
-            |dsn| {
-                let options = sentry::ClientOptions {
-                    traces_sample_rate: 1.0,
-                    sample_rate: 1.0,
-                    send_default_pii: true,
-                    attach_stacktrace: true,
-                    in_app_include: vec![crate_name!()],
-                    release: Some(Cow::Borrowed(crate_version!())),
-                    ..Default::default()
-                };
-                Some(sentry::init((dsn, options)))
-            },
-        )
+    pub fn init(&self) -> ClientInitGuard {
+        let options = sentry::ClientOptions {
+            traces_sample_rate: 1.0,
+            sample_rate: 1.0,
+            send_default_pii: true,
+            attach_stacktrace: true,
+            in_app_include: vec![crate_name!()],
+            release: Some(Cow::Borrowed(crate_version!())),
+            ..Default::default()
+        };
+        let guard = sentry::init((self.dsn.clone(), options));
+        if !guard.is_enabled() {
+            warn!("Sentry is disabled");
+        }
+        guard
     }
 }

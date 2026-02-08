294cb859c85dba023337ff119a8ffeefa4e50db1
Pavel Perestoronin <eigenein@gmail.com>
2026-02-08 20:10:29 +0100

    Delete dead migrations

--- migrations/0001_Create_meters_table.sql
+++
@@ -1,9 +0,0 @@
--- Migration number: 0001 	 2026-01-20T21:56:06.447Z
-CREATE TABLE meters (
-    timestamp                   INTEGER NOT NULL PRIMARY KEY,
-    p1_import_kwh               REAL NOT NULL,
-    p1_export_kwh               REAL NOT NULL,
-    battery_import_kwh          REAL NOT NULL,
-    battery_export_kwh          REAL NOT NULL,
-    battery_residual_energy_kwh REAL NOT NULL
-) WITHOUT ROWID, STRICT;

bda3e6ce76d3f1fe68f47e76b42aafdc373c0d55
Pavel Perestoronin <eigenein@gmail.com>
2026-03-30 18:38:46 +0200

    Simplify `Cron` 🚧

--- fennec-cli/src/cron.rs
+++ fennec-cli/src/cron.rs
@@ -9,22 +9,21 @@ pub struct CronSchedule(croner::Cron);
 
 impl CronSchedule {
     pub fn start(self) -> Cron {
-        Cron { schedule: self.0, last_timestamp: Local::now() }
+        Cron { schedule: self.0, pointer: Local::now() }
     }
 }
 
 pub struct Cron {
     schedule: croner::Cron,
-    last_timestamp: DateTime<Local>,
+    pointer: DateTime<Local>,
 }
 
 impl Cron {
     pub async fn wait_until_next(&mut self) -> Result {
         loop {
-            let next = self.schedule.find_next_occurrence(&self.last_timestamp, false)?;
-            self.last_timestamp = next;
-            if let Ok(duration) = (next - Local::now()).to_std() {
-                debug!(pattern = %self.schedule.pattern, ?next);
+            self.pointer = self.schedule.find_next_occurrence(&self.pointer, false)?;
+            if let Ok(duration) = (self.pointer - Local::now()).to_std() {
+                debug!(pattern = %self.schedule.pattern, next_timestamp = ?self.pointer, "sleeping…");
                 sleep(duration).await;
                 return Ok(());
             }

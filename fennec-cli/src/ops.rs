782aa996905cfc493c6708c045d1364b784c8248
Pavel Perestoronin <eigenein@gmail.com>
2026-05-12 19:50:00 +0200

    Move `Cache` to `cache` 🚧

--- fennec-cli/src/ops.rs
+++ fennec-cli/src/ops.rs
@@ -9,32 +9,12 @@ use chrono::TimeZone;
 
 pub use self::integrator::{BucketIntegrator, BucketMean, Integrator};
 use self::schedule::Interval;
-use crate::prelude::*;
 
 /// Simple one-value time-to-live cache.
 #[must_use]
 pub struct Cache<V> {
     time_to_live: Duration,
     entry: Option<cache::Entry<V>>,
-}
-
-impl<V> Cache<V> {
-    pub const fn new(time_to_live: Duration) -> Self {
-        Self { time_to_live, entry: None }
-    }
-
-    pub async fn get_or_insert_with(
-        &mut self,
-        init: impl Future<Output = Result<V>>,
-    ) -> Result<&V> {
-        if !matches!(
-            &self.entry,
-            Some(entry) if entry.timestamp.elapsed() <= self.time_to_live
-        ) {
-            self.entry = Some(cache::Entry::now(init.await?));
-        }
-        Ok(&self.entry.as_ref().unwrap().value)
-    }
 }
 
 /// Non-empty, ordered and continuous schedule.
--- fennec-cli/src/ops/cache.rs
+++ fennec-cli/src/ops/cache.rs
@@ -1,4 +1,28 @@
-use std::time::Instant;
+use std::time::{Duration, Instant};
+
+use crate::{
+    ops::{Cache, cache},
+    prelude::*,
+};
+
+impl<V> Cache<V> {
+    pub const fn new(time_to_live: Duration) -> Self {
+        Self { time_to_live, entry: None }
+    }
+
+    pub async fn get_or_insert_with(
+        &mut self,
+        init: impl Future<Output = Result<V>>,
+    ) -> Result<&V> {
+        if !matches!(
+            &self.entry,
+            Some(entry) if entry.timestamp.elapsed() <= self.time_to_live
+        ) {
+            self.entry = Some(cache::Entry::now(init.await?));
+        }
+        Ok(&self.entry.as_ref().unwrap().value)
+    }
+}
 
 #[must_use]
 pub struct Entry<T> {

f4f82056bbe3d34504b82306d26157a60dffa91a
Pavel Perestoronin <eigenein@gmail.com>
2026-05-30 20:30:28 +0200

    Clean up the dead code ⚰️

--- fennec-cli/src/quantity/energy.rs
+++ fennec-cli/src/quantity/energy.rs
@@ -18,25 +18,6 @@ implement_mul!(Watts, Hours, WattHours);
 
 impl WattHours {
     pub const ONE: Self = Self(1.0);
-
-    /// Project the value into a bucket index.
-    ///
-    /// TODO: I think I'll eventually use just integer [`WattHours`].
-    #[expect(clippy::cast_possible_truncation)]
-    #[expect(clippy::cast_sign_loss)]
-    pub const fn index(self, of: Self) -> usize {
-        let index = (of.0 / self.0).floor();
-        assert!(index >= 0.0);
-        index as usize
-    }
-
-    /// Un-project the bucket index into the value that represents the middle of the bucket.
-    ///
-    /// TODO: I think I'll eventually use just integer [`WattHours`].
-    #[expect(clippy::cast_precision_loss)]
-    pub const fn midpoint(self, index: usize) -> Self {
-        Self(self.0 * (index as f64 + 0.5))
-    }
 }
 
 impl From<usize> for WattHours {
@@ -118,21 +99,5 @@ impl From<KilowattHours> for WattHours {
 impl From<DecawattHours> for KilowattHours {
     fn from(decawatt_hours: DecawattHours) -> Self {
         Self(f64::from(decawatt_hours.0) * 0.01)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn energy_index() {
-        assert_eq!(WattHours(1.0).index(WattHours(3.0)), 3);
-        assert_eq!(WattHours(1.0).index(WattHours(3.0_f64.next_down())), 2);
-    }
-
-    #[test]
-    fn energy_midpoint() {
-        assert_eq!(WattHours(1.0).midpoint(2), WattHours(2.5));
     }
 }

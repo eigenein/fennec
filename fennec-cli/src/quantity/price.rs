a578644ed4e2faf6919fec809340bd77bc41dcc4
Pavel Perestoronin <eigenein@gmail.com>
2026-05-31 21:02:42 +0200

    Switch `KilowattHourPrice` to the new `Quantity` ♻️

--- fennec-cli/src/quantity/price.rs
+++ fennec-cli/src/quantity/price.rs
@@ -1,1 +1,9 @@
-quantity!(KilowattHourPrice, via: f64, suffix: "¤/kWh", precision: 3);
+use crate::quantity::{Format, Quantity};
+
+/// TODO: I'm unsure whether `M` should be `3` or `-3`.
+pub type KilowattHourPrice<V = f64> = Quantity<V, 3, -1, -1, 1>;
+
+impl Format for KilowattHourPrice {
+    const SUFFIX: &str = "¤/kWh";
+    const PRECISION: usize = 3;
+}

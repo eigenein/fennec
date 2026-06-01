4077867c064723443031bc62bee55f9c1c4f290d
Pavel Perestoronin <eigenein@gmail.com>
2026-06-01 21:24:23 +0200

    Replace battery efficiency structs with simple `Flow<f64>` ♻️

--- fennec-cli/src/battery.rs
+++ fennec-cli/src/battery.rs
@@ -1,21 +1,4 @@
-pub mod efficiency;
 mod simulator;
 mod working_mode;
 
 pub use self::{simulator::Simulator, working_mode::WorkingMode};
-
-#[must_use]
-#[derive(Copy, Clone)]
-pub struct Efficiency {
-    pub charging: f64,
-    pub discharging: f64,
-}
-
-impl Efficiency {
-    #[cfg(test)]
-    pub const IDEAL: Self = Self { charging: 1.0, discharging: 1.0 };
-
-    pub const fn round_trip(self) -> f64 {
-        self.charging * self.discharging
-    }
-}
--- fennec-cli/src/web/handlers/index.rs
+++ fennec-cli/src/web/handlers/index.rs
@@ -101,19 +101,19 @@ pub async fn get(State(state): State<web::State>) -> Markup {
                                 span.tag {
                                     span.icon-text {
                                         span.icon { i.fas.fa-rotate {} }
-                                        span { (format!("{:.1}%", 100.0 * energy_profile.battery_efficiency_estimator.as_efficiency().round_trip())) }
+                                        span { (format!("{:.1}%", 100.0 * energy_profile.battery_efficiency.round_trip())) }
                                     }
                                 }
                                 span.tag {
                                     span.icon-text {
                                         span.icon { i.fas.fa-angle-down {} }
-                                        span { (format!("{:.1}%", 100.0 * energy_profile.battery_efficiency_estimator.charging.0)) }
+                                        span { (format!("{:.1}%", 100.0 * energy_profile.battery_efficiency.import)) }
                                     }
                                 }
                                 span.tag {
                                     span.icon-text {
                                         span.icon { i.fas.fa-angle-up {} }
-                                        span { (format!("{:.1}%", 100.0 * energy_profile.battery_efficiency_estimator.discharging.0)) }
+                                        span { (format!("{:.1}%", 100.0 * energy_profile.battery_efficiency.export)) }
                                     }
                                 }
                             }

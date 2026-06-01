8e5c81204d783bd428e80c91180dc33c65fb3f00
Pavel Perestoronin <eigenein@gmail.com>
2026-06-01 21:02:54 +0200

    Get rid of the left-over `SmoothingFactor` ♻️

--- fennec-cli/src/math/smoothing.rs
+++ fennec-cli/src/math/smoothing.rs
@@ -18,35 +18,25 @@ pub struct Exponential<V>(
 
 impl<V> Exponential<V> {
     /// Update the value.
-    pub fn update(&mut self, value: V, smoothing_factor: SmoothingFactor)
+    pub fn update(&mut self, value: V, smoothing_factor: f64)
     where
         V: Clone + AddAssign + Sub<Output = V> + Mul<f64, Output = V>,
     {
-        self.0 += (value - self.0.clone()) * smoothing_factor.0;
+        self.0 += (value - self.0.clone()) * smoothing_factor;
     }
 }
 
-/// Exponential [smoothing factor][1].
-///
-/// Note: larger values of smoothing factor actually reduce the level of smoothing.
-///
-/// [1]: https://en.wikipedia.org/wiki/Exponential_smoothing#:~:text=available.%20The%20term-,smoothing%20factor,-applied%20to
-#[must_use]
-#[derive(Copy, Clone, derive_more::Debug)]
-#[debug("{_0}")]
-pub struct SmoothingFactor(f64);
-
-/// Half-life-parameterized exponential decay.
+/// Half-life of the exponential decay.
 #[must_use]
 #[derive(Copy, Clone)]
 pub struct HalfLife<V>(pub V);
 
 impl<V> HalfLife<V> {
     /// Calculate the smoothing factor from the quantity delta.
-    pub fn smoothing_factor(self, delta: V) -> SmoothingFactor
+    pub fn smoothing_factor(self, delta: V) -> f64
     where
         V: Div<V, Output = f64>,
     {
-        SmoothingFactor(-(-LN_2 * (delta / self.0)).exp_m1())
+        -(-LN_2 * (delta / self.0)).exp_m1()
     }
 }

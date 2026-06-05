d2dd5e2c757e4964509f9b80683bfc7a58ccab9c
Pavel Perestoronin <eigenein@gmail.com>
2026-06-05 18:52:36 +0200

    Silence the checks 🔊

--- fennec-cli/src/web/handlers/readiness.rs
+++ fennec-cli/src/web/handlers/readiness.rs
@@ -5,6 +5,6 @@ use crate::prelude::*;
 
 #[instrument(skip_all)]
 pub async fn get() -> impl IntoResponse {
-    info!("check");
+    debug!("check");
     (StatusCode::NO_CONTENT, [(header::CACHE_CONTROL, "no-cache, no-store, must-revalidate")])
 }

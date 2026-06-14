677109a669cae0ad2bf24baa3c2a8ef13f102abf
Pavel Perestoronin <eigenein@gmail.com>
2026-06-14 23:44:53 +0200

    Address `FIXME` 🚧

--- fennec-cli/src/solution/solved.rs
+++ fennec-cli/src/solution/solved.rs
@@ -11,12 +11,20 @@ pub struct Solved {
 }
 
 impl Solved {
-    pub fn reoptimize_state(
-        &self,
+    /// Re-optimize the state according to the current time and energy level.
+    ///
+    /// This strips the solution space off the past intervals and re-optimizes starting
+    /// with the currently running interval.
+    pub fn reoptimize(
+        &mut self,
         now: DateTime<Local>,
         initial_energy_level: EnergyLevel,
     ) -> Option<Solution> {
-        // FIXME: `now` may be already past the 0th interval.
+        self.space.pop_before(now);
+        if self.space.len() == 0 {
+            // TODO: we may need enum since empty space is not the same as missing solution.
+            return None;
+        }
         self.solver.optimize_state(now, 0, initial_energy_level, &self.space)
     }
 }

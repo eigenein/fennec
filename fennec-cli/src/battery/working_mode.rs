f7b0c6e77fd5ab875507cdf5f8e89c5c584047e9
Pavel Perestoronin <eigenein@gmail.com>
2026-06-23 18:26:12 +0200

    Drop `EnumSet` 🚧

--- fennec-cli/src/battery/working_mode.rs
+++ fennec-cli/src/battery/working_mode.rs
@@ -1,8 +1,6 @@
 use std::fmt::{Display, Formatter};
 
-use enumset::EnumSetType;
-
-#[derive(Debug, Hash, clap::ValueEnum, EnumSetType)]
+#[derive(Debug, Hash, Copy, Clone, Eq, PartialEq, clap::ValueEnum)]
 pub enum WorkingMode {
     /// Do not do anything.
     Idle,

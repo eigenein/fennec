85f246bdee1eec82466053ade37571eab3d9e616
Pavel Perestoronin <eigenein@gmail.com>
2026-07-09 20:44:00 +0200

    Move the general functions to the `mini_qube` root ♻️

--- fennec-modbus/src/contrib/mini_qube.rs
+++ fennec-modbus/src/contrib/mini_qube.rs
@@ -3,7 +3,11 @@
 //! [1]: https://fox-ess.uk/miniqube/
 //! [2]: https://www.solakon.de/products/solakon-one
 
-use crate::tcp;
+use crate::{
+    contrib::types::{DecawattHours, Percentage, Watts},
+    protocol::{address, function::ReadHoldingRegisters},
+    tcp,
+};
 
 pub mod functions;
 pub mod schedule;
@@ -11,3 +15,48 @@ pub mod types;
 
 /// Default unit ID ("slave ID") for MiniQube over direct TCP connection.
 pub const UNIT_ID: tcp::UnitId = tcp::UnitId::Significant(1);
+
+/// Read the battery state-of-health.
+pub type ReadStateOfHealth = ReadHoldingRegisters<address::Const<37624>, Percentage<u16>>;
+
+/// Read the battery design capacity.
+pub type ReadDesignCapacity = ReadHoldingRegisters<address::Const<37635>, DecawattHours<u16>>;
+
+/// Read the battery total active power (including EPS).
+pub type ReadTotalActivePower = ReadHoldingRegisters<address::Const<39134>, Watts<i32>>;
+
+/// Read the battery Emergency Power Supply active power.
+pub type ReadEpsActivePower = ReadHoldingRegisters<address::Const<39216>, Watts<i32>>;
+
+/// Read the battery state-of-charge.
+pub type ReadStateOfCharge = ReadHoldingRegisters<address::Const<39424>, Percentage<u16>>;
+
+/// Read the battery total energy exported to grid.
+pub type ReadTotalGridExportEnergy =
+    ReadHoldingRegisters<address::Const<39621>, DecawattHours<u32>>;
+
+/// Read the battery total energy imported from grid.
+pub type ReadTotalGridImportEnergy =
+    ReadHoldingRegisters<address::Const<39625>, DecawattHours<u32>>;
+
+/// Read the state-of-charge settings in a single transaction.
+///
+/// Reading three registers in one transaction reduces network latency and ensures atomic consistency
+/// compared to three separate read operations.
+pub type ReadStateOfChargeSettings =
+    ReadHoldingRegisters<address::Const<46609>, types::StateOfChargeSettings>;
+
+/// Read the system minimum allowed state-of-charge.
+///
+/// Unlike the reserve state-of-charge, this an absolute minimum for any battery state.
+pub type ReadMinimumSystemStateOfCharge =
+    ReadHoldingRegisters<address::Const<46609>, Percentage<u16>>;
+
+/// Read maximum allowed state-of-charge.
+pub type ReadMaximumStateOfCharge = ReadHoldingRegisters<address::Const<46610>, Percentage<u16>>;
+
+/// Read the minimum allowed state-of-charge in the on-grid mode.
+///
+/// This is also known as reserve state-of-charge.
+pub type ReadMinimumStateOfChargeOnGrid =
+    ReadHoldingRegisters<address::Const<46611>, Percentage<u16>>;
--- fennec-modbus/src/contrib/mini_qube/functions.rs
+++ fennec-modbus/src/contrib/mini_qube/functions.rs
@@ -13,96 +13,48 @@
 //! #[tokio::main]
 //! async fn main() -> Result<()> {
 //!     let state_of_charge = Client::new("battery.iot.home.arpa:502")
-//!         .call::<mini_qube::functions::ReadStateOfCharge>(mini_qube::UNIT_ID, address::Const)
+//!         .call::<mini_qube::ReadStateOfCharge>(mini_qube::UNIT_ID, address::Const)
 //!         .await?;
 //!     Ok(())
 //! }
 //! ```
 
-use super::{schedule, types};
-use crate::{
-    contrib::types::{DecawattHours, Percentage, Watts},
-    protocol::{
-        address,
-        function::{ReadHoldingRegisters, ReadWriteRegisters, WriteMultipleRegisters},
-    },
+use super::schedule;
+use crate::protocol::{
+    address,
+    function::{ReadHoldingRegisters, ReadWriteRegisters, WriteMultipleRegisters},
 };
 
-/// Read the battery state-of-health.
-pub type ReadStateOfHealth = ReadHoldingRegisters<address::Const<37624>, Percentage<u16>>;
-
-/// Read the battery design capacity.
-pub type ReadDesignCapacity = ReadHoldingRegisters<address::Const<37635>, DecawattHours<u16>>;
-
-/// Read the battery total active power (including EPS).
-pub type ReadTotalActivePower = ReadHoldingRegisters<address::Const<39134>, Watts<i32>>;
-
-/// Read the battery Emergency Power Supply active power.
-pub type ReadEpsActivePower = ReadHoldingRegisters<address::Const<39216>, Watts<i32>>;
-
-/// Read the battery state-of-charge.
-pub type ReadStateOfCharge = ReadHoldingRegisters<address::Const<39424>, Percentage<u16>>;
-
-/// Read the battery total energy exported to grid.
-pub type ReadTotalGridExportEnergy =
-    ReadHoldingRegisters<address::Const<39621>, DecawattHours<u32>>;
-
-/// Read the battery total energy imported from grid.
-pub type ReadTotalGridImportEnergy =
-    ReadHoldingRegisters<address::Const<39625>, DecawattHours<u32>>;
-
-/// Read the state-of-charge settings in a single transaction.
-///
-/// Reading three registers in one transaction reduces network latency and ensures atomic consistency
-/// compared to three separate read operations.
-pub type ReadStateOfChargeSettings =
-    ReadHoldingRegisters<address::Const<46609>, types::StateOfChargeSettings>;
-
-/// Read the system minimum allowed state-of-charge.
-///
-/// Unlike the reserve state-of-charge, this an absolute minimum for any battery state.
-pub type ReadMinimumSystemStateOfCharge =
-    ReadHoldingRegisters<address::Const<46609>, Percentage<u16>>;
-
-/// Read maximum allowed state-of-charge.
-pub type ReadMaximumStateOfCharge = ReadHoldingRegisters<address::Const<46610>, Percentage<u16>>;
-
-/// Read the minimum allowed state-of-charge in the on-grid mode.
-///
-/// This is also known as reserve state-of-charge.
-pub type ReadMinimumStateOfChargeOnGrid =
-    ReadHoldingRegisters<address::Const<46611>, Percentage<u16>>;
-
-/// Read schedule entry.
+/// Read schedule slot.
 ///
 /// This function accepts the slot index as the argument.
 ///
-/// If you're reading the complete schedule, consider calling [`ReadScheduleEntryBlock`] instead.
+/// If you're reading the complete schedule, consider calling [`ReadScheduleSlotBlock`] instead.
 pub type ReadScheduleEntry = ReadHoldingRegisters<
     address::Stride<48010, { schedule::Slot::N_TOTAL }, schedule::Slot>,
     schedule::Slot,
 >;
 
-/// Read 12 schedule entries at a time.
-pub type ReadScheduleEntryBlock = ReadHoldingRegisters<schedule::BlockIndex, schedule::Block>;
+/// Read 12 schedule slots at a time.
+pub type ReadScheduleSlotBlock = ReadHoldingRegisters<schedule::BlockIndex, schedule::Block>;
 
-/// Write schedule entry.
+/// Write schedule slot.
 ///
 /// This function accepts the slot index as the argument.
 ///
-/// If you're writing the complete schedule, consider calling [`WriteScheduleEntryBlock`] instead.
-pub type WriteScheduleEntry = WriteMultipleRegisters<
+/// If you're writing the complete schedule, consider calling [`WriteScheduleSlotBlock`] instead.
+pub type WriteScheduleSlot = WriteMultipleRegisters<
     address::Stride<48010, { schedule::Slot::N_TOTAL }, schedule::Slot>,
     schedule::Slot,
 >;
 
 /// Write 12 schedule entries at a time.
-pub type WriteScheduleEntryBlock = WriteMultipleRegisters<schedule::BlockIndex, schedule::Block>;
+pub type WriteScheduleSlotBlock = WriteMultipleRegisters<schedule::BlockIndex, schedule::Block>;
 
 /// Write and read 12 schedule entries at a time.
 ///
-/// Note: Fox ESS MQ2200 returns "illegal function" for this one.
-pub type ReadWriteScheduleEntryBlock = ReadWriteRegisters<
+/// Note: Fox ESS MQ2200 returns "illegal function" with incorrect function code for this one.
+pub type ReadWriteScheduleSlotBlock = ReadWriteRegisters<
     schedule::BlockIndex,
     schedule::Block,
     schedule::BlockIndex,
